4597d7d266a3649950a804af39b7e936f3a6e472
Adam Różyński <adam@micropixels.pl>
2023-04-11 23:26:21 +0200

    Initial Commit

---
+++ BatFi/BatFiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatFiApp.swift
+//  BatFi
+//
+//  Created by Adam on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BatFiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
