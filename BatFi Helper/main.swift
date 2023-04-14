2e071611b64188e9b6d29407fbaf7a071ecc1010
Adam Różyński <adam@micropixels.pl>
2023-04-14 22:21:58 +0200

    Remove unneeded stuff, configure the project

---
+++ BatFi Helper/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  BatFi Helper
+//
+//  Created by Adam on 13/04/2023.
+//
+
+import Foundation
+
+func main() {
+    while true {
+        print("Hello Cruel World!")
+        sleep(10)
+    }
+}
+
+main()
--- BatFi/BatFiApp.swift
+++ BatFi/BatFiApp.swift
@@ -5,13 +5,42 @@
 //  Created by Adam on 11/04/2023.
 //
 
+import ServiceManagement
 import SwiftUI
 
 @main
 struct BatFiApp: App {
+    @State private var chargingDisabled = false
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra {
+            Button {
+                Task.detached {
+                    let service = SMAppService.daemon(plistName: "software.micropixels.BatFi.helper.plist")
+                    do {
+                        print("will register, status: \(service.status.rawValue)")
+                        try service.register()
+                        print("did register")
+                    } catch {
+                        print("did not register, error: \(error)")
+                    }
+                }
+            } label: {
+                Text("Install helper tool")
+            }
+        } label: {
+            if chargingDisabled {
+                Image(systemName: "minus.plus.batteryblock")
+            } else {
+                Image(systemName: "bolt.batteryblock.fill")
+
+            }
         }
+        .onChange(of: chargingDisabled) { disable in
+            // sudo smc -k CH0B -w 02
+            // sudo smc -k CH0C -w 02
+           
+        }
+
     }
 }
