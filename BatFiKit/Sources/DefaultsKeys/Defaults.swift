65ed7b96d227cd9b839606f8bbc643a88c562509
Adam Różyński <adam@micropixels.pl>
2023-06-12 17:32:44 +0200

    Show the debug menu by default in the dev version

--- BatFiKit/Sources/DefaultsKeys/Defaults.swift
+++ BatFiKit/Sources/DefaultsKeys/Defaults.swift
@@ -8,6 +8,12 @@
 import Defaults
 import Foundation
 
+#if DEBUG
+private let showDebugMenuByDefault = true
+#else
+private let showDebugMenuByDefault = false
+#endif
+
 extension Defaults.Keys {
     public static let launchAtLogin = Key<Bool>("launchAtLogin", default: true)
     public static let chargeLimit = Key<Int>("chargeLimit", default: 80)
@@ -20,7 +26,8 @@ extension Defaults.Keys {
     public static let onboardingIsDone = Key<Bool>("onboardingIsDone", default: false)
     public static let monochromeStatusIcon = Key<Bool>("monochromeStatusIcon", default: true)
     public static let showBatteryPercentageInStatusIcon = Key<Bool>("showBatteryPercentageInStatusIcon", default: true)
-    public static let showDebugMenu = Key<Bool>("showDebugMenu", default: false)
+    public static let showDebugMenu = Key<Bool>("showDebugMenu", default: showDebugMenuByDefault)
+
 
     // notifications
     public static let showChargingStausChanged = Key<Bool>("showChargingStausChanged", default: true)
