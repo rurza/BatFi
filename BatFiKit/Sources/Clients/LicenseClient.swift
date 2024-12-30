8971efb9081c560700e6dbda7e4f2cdd2da96898
Adam Różyński <adam@micropixels.pl>
2024-12-31 00:42:24 +0100

    Add LicenseClient

--- BatFiKit/Sources/Clients/LicenseClient.swift
+++ BatFiKit/Sources/Clients/LicenseClient.swift
@@ -1,19 +1,31 @@
 import Dependencies
 import Foundation
+import DependenciesMacros
 
+@DependencyClient
 public struct LicenseClient: TestDependencyKey {
     public var checkLicense: (_ email: String, _ key: String) async throws -> License
-
-    init(checkLicense: @escaping (String, String) async throws -> License) {
-        self.checkLicense = checkLicense
-    }
 
     public static var testValue: LicenseClient = unimplemented()
 }
 
-public struct License: Codable, Equatable {
+public struct License: Equatable {
     public let key: String
     public let name: String?
     public let email: String
     public let purchaseDate: Date
+
+    public init(key: String, name: String?, email: String, purchaseDate: Date) {
+        self.key = key
+        self.name = name
+        self.email = email
+        self.purchaseDate = purchaseDate
+    }
+}
+
+public extension DependencyValues {
+    var licenseClient: LicenseClient {
+        get { self[LicenseClient.self] }
+        set { self[LicenseClient.self] = newValue }
+    }
 }
---
+++ BatFiKit/Sources/Shared/Error+String.swift
@@ -0,0 +1,12 @@
+//
+//  Error+String.swift
+//  BatFiKit
+//
+//  Created by Adam Różyński on 29.12.2024.
+//
+
+import Foundation
+
+extension String: @retroactive Error {
+    
+}
