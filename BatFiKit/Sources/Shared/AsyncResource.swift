ac6120f4975db5480fb328328c6b53d78d7f697f
Adam Różyński <adam@micropixels.pl>
2025-01-12 00:00:04 +0100

    Add KeychainClient

---
+++ BatFiKit/Sources/Shared/AsyncResource.swift
@@ -0,0 +1,40 @@
+//
+//  AsyncResource.swift
+//  BatFiKit
+//
+//  Created by Adam Różyński on 11.01.2025.
+//
+
+import Foundation
+
+public enum AsyncResource<Resource> {
+    case initial
+    case loading
+    case loaded(Resource)
+    case error(NSError)
+
+    var isLoading: Bool {
+        switch self {
+        case .loading: return true
+        default: return false
+        }
+    }
+
+    var error: NSError? {
+        switch self {
+        case .error(let error): return error
+        default: return nil
+        }
+    }
+
+    var resource: Resource? {
+        switch self {
+        case .loaded(let resource): return resource
+        default: return nil
+        }
+    }
+}
+
+extension AsyncResource: Equatable where Resource: Equatable { }
+
+extension AsyncResource: Hashable where Resource: Hashable { }
