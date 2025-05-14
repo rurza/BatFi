808aec74560abf94ad3e247603cde135ae8a8d4f
Adam Różyński <adam@micropixels.pl>
2025-05-14 22:53:17 +0200

    Update license video filename

--- BatFiKit/Sources/Onboarding/OnboardingPlayerViewModel.swift
+++ BatFiKit/Sources/Onboarding/OnboardingPlayerViewModel.swift
@@ -19,7 +19,7 @@ private extension OnboardingScreen {
         case .charging:
             return "usage"
         case .license:
-            return "license"
+            return "license_v2"
         }
     }
 }
