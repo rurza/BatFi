6f8e8b7d92605573ac1eea316532ba26de383c1c
Adam Różyński <adam@micropixels.pl>
2025-09-08 23:10:28 +0200

    Revert "Fix menu resizing"
    This reverts commit 55217934239c7c2030c26dfd4ccb3d2790859894.

--- BatFiKit/Sources/AppCore/MenuContent.swift
+++ BatFiKit/Sources/AppCore/MenuContent.swift
@@ -7,7 +7,6 @@
 
 import AppShared
 import BatteryInfo
-import Combine
 import Defaults
 import DefaultsKeys
 import HighEnergyUsage
@@ -18,7 +17,6 @@ import SharedUI
 import SwiftUI
 
 struct MenuContent: View {
-    var sizePassthrough: PassthroughSubject<CGSize, Never>
     @ObservedObject var licenseModel: LicenseModel
 
     var body: some View {
@@ -44,22 +42,5 @@ struct MenuContent: View {
                 SeparatorView()
             }
         }
-        .overlay(
-            GeometryReader { geometryProxy in
-                Color.clear
-                    .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
-            }
-        )
-        .onPreferenceChange(
-            SizePreferenceKey.self,
-            perform: { size in
-                sizePassthrough.send(size)
-            }
-        )
     }
-}
-
-private struct SizePreferenceKey: PreferenceKey {
-    static var defaultValue: CGSize = .zero
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) { value = nextValue() }
 }
